e4b810b6a22ae37348f2c7dfb28b49023e9c4207
Santiago Garcia <sgarciavil.29@gmail.com>
2021-10-29 14:09:12 -0500

    Pagina about

--- client/src/pages/about.vue
+++ client/src/pages/about.vue
@@ -2,5 +2,9 @@
 
 </script>
 <template>
-    <h1>About</h1>
+    <h1 class="text-4xl">About</h1>
+
+    <p class="my-2">Year Summary was created to make a version of the Wrapped event of Spotify year-round it uses the <a href="https://developer.spotify.com/" class="font-bold underline-light-50 underline">Spotify API</a>. </p>
+
+    <p class="my-2"> To get the <span class="font-bold">Top artists/tracks:</span> we recive a token after the user logins with his/hers credentials then, with that token requests are made to get the top artists/tracks and are parsed to the table.</p>
 </template>
