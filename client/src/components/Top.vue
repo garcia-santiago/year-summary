81df3c7a88e30074e7637f977c6e084d0110093c
Santiago Garcia <sgarciavil.29@gmail.com>
2021-10-29 14:08:57 -0500

    Añadido iconos para ir a la pagina de about y home

--- client/src/components/Top.vue
+++ client/src/components/Top.vue
@@ -4,6 +4,11 @@ import { isDark, toggleDark } from '~/logic'
 
 <template>
   <nav class="text-xl mt-6">
+
+    <a class="icon-btn mx-2" rel="noreferrer" href="/" title="Home">
+      <carbon-home />
+    </a>
+
     <button class="icon-btn mx-2 !outline-none" @click="toggleDark">
       <carbon-moon v-if="!isDark" />
       <carbon-sun v-else />
@@ -12,5 +17,10 @@ import { isDark, toggleDark } from '~/logic'
     <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/garcia-santiago/" target="_blank" title="GitHub">
       <carbon-logo-github />
     </a>
+
+    <a class="icon-btn mx-2" rel="noreferrer" href="/about" title="About">
+      <carbon-help />
+    </a>
+
   </nav>
 </template>
