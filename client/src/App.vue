64ecc36aa1558a8832c6f7f0f248f18158c04a67
Santiago Garcia <sgarciavil.29@gmail.com>
2021-10-29 14:08:30 -0500

    Añadido footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,14 @@
+<script setup lang="ts">
+import MyFooter from "./components/MyFooter.vue"
+
+</script>
 <template>
-  <main class="px-4 py-10 text-center">
+  <main class="text-center">
     <Top />
+
     <router-view />
+
+    <MyFooter />
   </main>
 </template>
 
---
+++ client/src/components/MyFooter.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <footer class="sticky bottom-3 mt-30">
+         <span>Year Summary by <a href="https://sgarcia1.herokuapp.com/" class="font-bold underline">Santiago García</a> </span>
+    </footer>
+</template>
