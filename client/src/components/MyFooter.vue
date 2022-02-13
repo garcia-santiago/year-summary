361c0f818bab78407c5462c8a55e5b726755d0fe
Santiago Garcia <sgarciavil.29@gmail.com>
2022-02-12 22:30:25 -0500

    Quitado sticky al footer

--- client/src/components/MyFooter.vue
+++ client/src/components/MyFooter.vue
@@ -2,7 +2,7 @@
 
 </script>
 <template>
-    <footer class="sticky bottom-3 mt-30">
+    <footer class="mt-30">
          <span>Year Summary by <a href="https://sgarcia1.herokuapp.com/" class="font-bold underline">Santiago García</a> </span>
     </footer>
 </template>
